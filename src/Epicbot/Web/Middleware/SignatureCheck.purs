33b30f2ae2d1b28d92ce0805cc12437836cc743a
drew <drew@drewolson.org>
2020-01-31 14:35:03 -0600

    Remove unneeded token functions

--- src/Epicbot/Web/Middleware/SignatureCheck.purs
+++ src/Epicbot/Web/Middleware/SignatureCheck.purs
@@ -4,23 +4,14 @@ module Epicbot.Web.Middleware.SignatureCheck
 
 import Prelude
 
-import Control.Alt ((<|>))
 import Control.Monad.Logger.Class (info)
 import Control.Monad.Reader (ask)
-import Data.Argonaut.Core (Json)
-import Data.Argonaut.Decode (decodeJson, (.:))
-import Data.Argonaut.Parser (jsonParser)
-import Data.Either (Either, hush)
-import Data.HashMap (HashMap)
-import Data.HashMap as HashMap
 import Data.Log.Tag (empty)
 import Data.Maybe (Maybe(..))
 import Epicbot.App (ResponseM)
 import Epicbot.Slack.Signature as Signature
-import Epicbot.Web.Body as Body
 import HTTPure ((!!))
 import HTTPure as HTTPure
-import HTTPure.Utils (urlDecode)
 
 call :: (HTTPure.Request -> ResponseM) -> HTTPure.Request -> ResponseM
 call router req@{ body, headers } = do
@@ -39,24 +30,5 @@ call router req@{ body, headers } = do
           HTTPure.unauthorized
 
     _, _ -> do
-      info empty "No token found"
+      info empty "No signature found"
       HTTPure.unauthorized
-
-tokenLookup :: String -> Maybe String
-tokenLookup body =
-  let parsedBody = Body.asHashMap body
-   in HashMap.lookup "token" parsedBody <|> tokenFromPayload parsedBody
-
-tokenFromPayload :: HashMap String String -> Maybe String
-tokenFromPayload parsedBody =
-  parseToken =<< urlDecode <$> HashMap.lookup "payload" parsedBody
-
-parseToken :: String -> Maybe String
-parseToken payload = hush $ decodeToken =<< jsonParser payload
-
-decodeToken :: Json -> Either String String
-decodeToken json = do
-  obj <- decodeJson json
-  token <- obj .: "token"
-
-  pure token
