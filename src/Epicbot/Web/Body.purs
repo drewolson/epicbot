b0779e5ab3b41c068c99493a08b563029cf7bafb
drew <drew@drewolson.org>
2020-03-23 18:11:46 -0500

    Use FormURLEncoded rather than a custom parser

--- src/Epicbot/Web/Body.purs
+++ src/Epicbot/Web/Body.purs
@@ -4,49 +4,14 @@ module Epicbot.Web.Body
 
 import Prelude
 
-import Control.Alt ((<|>))
-import Data.Either (hush)
-import Data.Foldable (class Foldable)
-import Data.Foldable as Foldable
+import Data.FormURLEncoded as FormURLEncoded
 import Data.Map (Map)
 import Data.Map as Map
-import Data.List (List(..))
-import Data.List as List
-import Data.Maybe (Maybe(..), fromMaybe)
-import Data.String.CodeUnits as CodeUnits
-import Data.Tuple (Tuple(..))
-import Text.Parsing.StringParser (Parser, runParser, try)
-import Text.Parsing.StringParser.CodePoints (anyChar, oneOf, eof, string)
-import Text.Parsing.StringParser.Combinators (lookAhead, manyTill, sepBy)
-
-asString :: forall f. Foldable f => f Char -> String
-asString = Foldable.foldMap CodeUnits.singleton
-
-endOfEntry :: Parser Unit
-endOfEntry = lookAhead $ (oneOf ['=', '&'] *> pure unit) <|> eof
-
-parseToken :: Parser String
-parseToken = asString <$> manyTill anyChar endOfEntry
-
-parseGoodEntry :: Parser (Tuple String String)
-parseGoodEntry = do
-  key <- parseToken <* string "="
-  val <- parseToken
-
-  pure $ Tuple key val
-
-parseBadEntry :: Parser Unit
-parseBadEntry = parseToken *> pure unit
-
-parseEntry :: Parser (Maybe (Tuple String String))
-parseEntry =
-  try (Just <$> parseGoodEntry) <|> (parseBadEntry *> pure Nothing)
-
-parseQuery :: Parser (List (Tuple String String))
-parseQuery = do
-  entries <- sepBy parseEntry $ string "&"
-
-  pure $ List.catMaybes entries
+import Data.Maybe as Maybe
 
 asMap :: String -> Map String String
-asMap = Map.fromFoldable <<< fromMaybe Nil <<< hush <<< runParser parseQuery
+asMap =
+  Map.mapMaybe identity
+  <<< Map.fromFoldable
+  <<< Maybe.maybe [] FormURLEncoded.toArray
+  <<< FormURLEncoded.decode
