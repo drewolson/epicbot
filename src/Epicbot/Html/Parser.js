2867102bc140fe0fe3592feeed1d3032aea13fa3
drew <drew@drewolson.org>
2020-06-12 22:09:42 -0500

    Add eslint

--- src/Epicbot/Html/Parser.js
+++ src/Epicbot/Html/Parser.js
@@ -3,28 +3,33 @@ const cheerio = require("cheerio");
 exports.parseCards = function (doc) {
   var $ = cheerio.load(doc);
 
-  return $("tr").map(function (i, elem) {
-    var id = i.toString();
-    var name = $(this).find("td.column-2").text()
+  return $("tr")
+    .map(function (i) {
+      var id = i.toString();
+      var name = $(this).find("td.column-2").text();
 
-    if (name === "") {
-      return null;
-    }
-
-    var urls = $(this).find("img").map(function (j, nested) {
-      var src = $(this).attr("src");
-
-      if (src.endsWith("epic_back.jpg")) {
+      if (name === "") {
         return null;
       }
 
-      return src;
-    }).get();
+      var urls = $(this)
+        .find("img")
+        .map(function () {
+          var src = $(this).attr("src");
 
-    return {
-      id: id,
-      name: name,
-      urls: urls
-    };
-  }).get();
+          if (src.endsWith("epic_back.jpg")) {
+            return null;
+          }
+
+          return src;
+        })
+        .get();
+
+      return {
+        id: id,
+        name: name,
+        urls: urls,
+      };
+    })
+    .get();
 };
