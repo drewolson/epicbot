2a264a2c21bedc5fbe12ad18b94447dd3e660222
drew <drew@drewolson.org>
2020-06-15 08:30:27 -0500

    Remove unused parsing helpers

--- src/Epicbot/Slack/Command.purs
+++ src/Epicbot/Slack/Command.purs
@@ -4,15 +4,11 @@ module Epicbot.Slack.Command
   ) where
 
 import Prelude
-import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
-import Data.List (List)
-import Data.String.CodePoints as CodePoints
 import Text.Parsing.StringParser (Parser, runParser)
-import Text.Parsing.StringParser.CodePoints (anyChar, eof, string, whiteSpace)
-import Text.Parsing.StringParser.Combinators (many)
+import Text.Parsing.StringParser.CodePoints (eof, string, whiteSpace)
 
 data Command
   = Draft
@@ -25,15 +21,6 @@ derive instance genericCommand :: Generic Command _
 instance showCommand :: Show Command where
   show :: Command -> String
   show = genericShow
-
-charsToString :: List Char -> String
-charsToString =
-  CodePoints.fromCodePointArray
-    <<< Array.fromFoldable
-    <<< map CodePoints.codePointFromChar
-
-anyString :: Parser String
-anyString = charsToString <$> many anyChar
 
 parseDraft :: Parser Command
 parseDraft = Draft <$ (string "draft" <* whiteSpace <* eof)
