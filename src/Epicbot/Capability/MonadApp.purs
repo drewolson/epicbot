5b2badd4f1677f41ca179fed75742965e54c9a87
drew <drew@drewolson.org>
2021-04-18 10:38:24 -0500

    MonadApp does not include MonadTime

--- src/Epicbot/Capability/MonadApp.purs
+++ src/Epicbot/Capability/MonadApp.purs
@@ -6,12 +6,10 @@ import Control.Monad.Logger.Class (class MonadLogger)
 import Effect.Aff.Class (class MonadAff)
 import Epicbot.Capability.Has (class Has)
 import Epicbot.Capability.MonadSignature (class MonadSignature)
-import Epicbot.Capability.MonadTime (class MonadTime)
 import Epicbot.Index (Index)
 
 class
   ( Has Index m
-  , MonadTime m
   , MonadSignature m
   , MonadAff m
   , MonadLogger m
